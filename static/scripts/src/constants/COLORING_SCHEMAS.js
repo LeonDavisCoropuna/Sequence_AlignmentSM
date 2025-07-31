3ea305bdfb8327b5d676bafb4ec01f1a86764328
Leon Davis <ldavis@unsa.edu.pe>
2025-07-30 19:31:39 -0500

    change logo

--- static/scripts/src/constants/COLORING_SCHEMAS.js
+++ static/scripts/src/constants/COLORING_SCHEMAS.js
@@ -21,6 +21,29 @@ export const DEFAULT_PROTEIN_COLORING_SCHEMA = {
   "Y": "#d1a7ff"
 };
 
+export const PAM250_PROTEIN_COLOR_SCHEMA = {
+  "A": "#9bbb67",
+  "C": "#0fc4ff",
+  "D": "#2daa07",
+  "E": "#3e9901",
+  "F": "#fe700e",
+  "G": "#56e226",
+  "H": "#627f54",
+  "I": "#e6c356",
+  "K": "#628101",
+  "L": "#ffb209",
+  "M": "#f0c106",
+  "N": "#77984a",
+  "P": "#10e299",
+  "Q": "#478624",
+  "R": "#118768",
+  "S": "#91ae85",
+  "T": "#a7c587",
+  "V": "#d5ca1c",
+  "W": "#da239b",
+  "Y": "#c65503"
+}
+
 export const DEFAULT_NUCLEIC_COLORING_SCHEMA = {
   "A": "#4169E1",
   "C": "#FF4500",
